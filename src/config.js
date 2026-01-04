82b9908a2a1d63f6add43866b5df1cdb9ef820ae
u2key <248891400+u2key@users.noreply.github.com>
2026-01-04 18:58:20 +0900

    Update src/config.js
    Comments removed

--- src/config.js
+++ src/config.js
@@ -1,32 +1,29 @@
-// 設定を記載しておくクラス
-class Config {
-}
-Config.puyoImgWidth = 40; // ぷよぷよ画像の幅
-Config.puyoImgHeight = 40; // ぷよぷよ画像の高さ
+let Config = {};
+
+Config.puyoImgWidth = 40;
+Config.puyoImgHeight = 40;
 
 Config.fontHeight = 33;
 
-Config.stageCols = 6; // ステージの横の個数
-Config.stageRows = 12; // ステージの縦の個数
+Config.stageCols = 6;
+Config.stageRows = 12;
 
-// フィールドサイズ追加
-// 高さが全部入るように調整
 Config.puyoImgHeight = (window.innerHeight-Config.fontHeight)/Config.stageRows
 Config.puyoImgWidth = Config.puyoImgHeight;
 
-Config.stageBackgroundColor = '#ffffff'; // ステージの背景色
-Config.scoreBackgroundColor = '#24c0bb'; // スコアの背景色
+Config.stageBackgroundColor = '#ffffff';
+Config.scoreBackgroundColor = '#24c0bb';
 
-Config.freeFallingSpeed = 16; // 自由落下のスピード
-Config.erasePuyoCount = 4; // 何個以上揃ったら消えるか
-Config.eraseAnimationDuration = 30; // 何フレームでぷよを消すか
+Config.freeFallingSpeed = 16;
+Config.erasePuyoCount = 4;
+Config.eraseAnimationDuration = 30;
 
-Config.puyoColors = 4; // 何色のぷよを使うか
-Config.playerFallingSpeed = 0.9; // プレイ中の自然落下のスピード
-Config.playerDownSpeed = 15; // プレイ中の下キー押下時の落下スピード
-Config.playerGroundFrame = 20; // 何フレーム接地したらぷよを固定するか
-Config.playerMoveFrame = 10; // 左右移動に消費するフレーム数
-Config.playerRotateFrame = 10; // 回転に消費するフレーム数
+Config.puyoColors = 4;
+Config.playerFallingSpeed = 0.9;
+Config.playerDownSpeed = 15;
+Config.playerGroundFrame = 20;
+Config.playerMoveFrame = 10;
+Config.playerRotateFrame = 10;
 
-Config.zenkeshiDuration = 150; // 全消し時のアニメーションミリセカンド
-Config.gameOverFrame = 3000; // ゲームオーバー演出のサイクルフレーム
+Config.zenkeshiDuration = 150;
+Config.gameOverFrame = 3000;
