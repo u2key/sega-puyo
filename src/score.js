b791e454bac12c75e6079d8a2bfe56700c58bc63
u2key <248891400+u2key@users.noreply.github.com>
2026-01-05 01:16:02 +0900

    Update src/score.js
    Initialize process changed.

--- src/score.js
+++ src/score.js
@@ -1,32 +1,36 @@
 class Score {
   static initialize() {
+    this.rensaBonus = [0, 8, 16, 32, 64, 96, 128, 160, 192, 224, 256, 288, 320, 352, 384, 416, 448, 480, 512, 544, 576, 608, 640, 672];
+    this.pieceBonus = [0, 0, 0, 0, 2, 3, 4, 5, 6, 7, 10, 10];
+    this.colorBonus = [0, 0, 3, 6, 12, 24];
     this.fontTemplateList = [];
-    let fontWidth = 0;
-    for (let i = 0; i < 10; i++) {
-      const fontImage = document.getElementById(`font${i}`);
-      if (fontWidth === 0) {
-        fontWidth = fontImage.width / fontImage.height * Config.fontHeight;
-      }
-      fontImage.height = Config.fontHeight;
-      fontImage.width = fontWidth;
-      this.fontTemplateList.push(fontImage);
+    for (let a = 0; a < 10; a++) {
+      this.fontTemplateList.push(new Image());
+      Game.loadImg(`img/${a}.png`, this.fontTemplateList[a], () => {
+        this.fontTemplateList[a].height = Config.fontHeight;
+        this.fontTemplateList[a].width = this.fontTemplateList[a].width / this.fontTemplateList[a].height * Config.fontHeight;
+      });
     }
-    this.fontLength = Math.floor(Config.stageCols * Config.puyoImgWidth / this.fontTemplateList[0].width);
+  }
+  
+  static start() {
+    this.fontLength = Math.floor(Config.stageCols * Config.puyoImgWidth / Score.fontTemplateList[0].width);
     this.score = 0;
     this.showScore();
   }
+  
   static showScore () {
     let score = this.score;
-    const scoreElement = Stage.scoreElement;
-    while (scoreElement.firstChild) {
-      scoreElement.removeChild(scoreElement.firstChild);
+    while (Stage.scoreElement.firstChild) {
+      Stage.scoreElement.removeChild(Stage.scoreElement.firstChild);
     }
-    for (let i = 0; i < this.fontLength; i++) {
+    for (let a = 0; a < this.fontLength; a++) {
       const number = score % 10;
-      scoreElement.insertBefore(this.fontTemplateList[number].cloneNode(true), scoreElement.firstChild);
+      Stage.scoreElement.insertBefore(this.fontTemplateList[number].cloneNode(true), Stage.scoreElement.firstChild);
       score = Math.floor(score / 10);
     }
   }
+  
   static calculateScore (rensa, piece, color) {
     rensa = Math.min(rensa, Score.rensaBonus.length - 1);
     piece = Math.min(piece, Score.pieceBonus.length - 1);
@@ -37,12 +41,9 @@ class Score {
     }
     this.addScore(scale * piece * 10);
   }
+  
   static addScore (score) {
     this.score += score;
     this.showScore();
   }
-};
-
-Score.rensaBonus = [0, 8, 16, 32, 64, 96, 128, 160, 192, 224, 256, 288, 320, 352, 384, 416, 448, 480, 512, 544, 576, 608, 640, 672];
-Score.pieceBonus = [0, 0, 0, 0, 2, 3, 4, 5, 6, 7, 10, 10];
-Score.colorBonus = [0, 0, 3, 6, 12, 24];
+}
