2b9621f8396c09c472d45924e824463264ea5265
u2key <248891400+u2key@users.noreply.github.com>
2026-01-04 22:08:19 +0900

    Update src/game.js
    Change initialize function

--- src/game.js
+++ src/game.js
@@ -1,93 +1,73 @@
-window.addEventListener("load", () => {
-  initialize();
-  loop();
-});
-
-let mode;
-let frame;
+let mode = 'start';
+let frame = 0;
 let combinationCount = 0;
 
-function initialize() {
+window.addEventListener("load", function () {
   PuyoImage.initialize();
   Stage.initialize();
   Player.initialize();
   Score.initialize();
-  mode = 'start';
-  frame = 0;
-}
+  loop();
+});
 
 function loop() {
-  switch (mode) {
-    case 'start':
+  if (mode == 'start') {
+    mode = 'checkFall';
+  } else if (mode == 'checkFall') {
+    if (Stage.checkFall()) {
+      mode = 'fall'
+    } else {
+      mode = 'checkErase';
+    }
+  } else if (mode == 'fall') {
+    if (!Stage.fall()) {
+      mode = 'checkErase';
+    }
+  } else if (mode == 'checkErase') {
+    const eraseInfo = Stage.checkErase(frame);
+    if (eraseInfo) {
+      mode = 'erasing';
+      combinationCount++;
+      Score.calculateScore(combinationCount, eraseInfo.piece, eraseInfo.color);
+      Stage.hideZenkeshi();
+    } else {
+      if (Stage.puyoCount === 0 && combinationCount > 0) {
+        Stage.showZenkeshi();
+        Score.addScore(3600);
+      }
+      combinationCount = 0;
+      mode = 'newPuyo'
+    }
+  } else if (mode == 'erasing') {
+    if (!Stage.erasing(frame)) {
       mode = 'checkFall';
-      break;
-    case 'checkFall':
-      if (Stage.checkFall()) {
-        mode = 'fall'
-      } else {
-        mode = 'checkErase';
-      }
-      break;
-    case 'fall':
-      if (!Stage.fall()) {
-        mode = 'checkErase';
-      }
-      break;
-    case 'checkErase':
-      const eraseInfo = Stage.checkErase(frame);
-      if (eraseInfo) {
-        mode = 'erasing';
-        combinationCount++;
-        Score.calculateScore(combinationCount, eraseInfo.piece, eraseInfo.color);
-        Stage.hideZenkeshi();
-      } else {
-        if (Stage.puyoCount === 0 && combinationCount > 0) {
-          Stage.showZenkeshi();
-          Score.addScore(3600);
-        }
-        combinationCount = 0;
-        mode = 'newPuyo'
-      }
-      break;
-    case 'erasing':
-      if (!Stage.erasing(frame)) {
-        mode = 'checkFall';
-      }
-      break;
-    case 'newPuyo':
-      if (!Player.createNewPuyo()) {
-        mode = 'gameOver';
-      } else {
-        mode = 'playing';
-      }
-      break;
-    case 'playing':
-      const action = Player.playing(frame);
-      mode = action;
-      break;
-    case 'moving':
-      if (!Player.moving(frame)) {
-        mode = 'playing';
-      }
-      break;
-    case 'rotating':
-      if (!Player.rotating(frame)) {
-        mode = 'playing';
-      }
-      break;
-    case 'fix':
-      Player.fix();
-      mode = 'checkFall'
-      break;
-    case 'gameOver':
-      PuyoImage.prepareBatankyu(frame);
-      mode = 'batankyu';
-      break;
-    case 'batankyu':
-      PuyoImage.batankyu(frame);
-      Player.batankyu();
-      break;
+    }
+  } else if (mode == 'newPuyo') {
+    if (!Player.createNewPuyo()) {
+      mode = 'gameOver';
+    } else {
+      mode = 'playing';
+    }
+  } else if (mode == 'playing') {
+      mode = Player.playing(frame);
+  } else if (mode == 'moving') {
+    if (!Player.moving(frame)) {
+      mode = 'playing';
+    }
+  } else if (mode == 'rotating') {
+    if (!Player.rotating(frame)) {
+      mode = 'playing';
+    }
+  } else if (mode == 'fix') {
+    Player.fix();
+    mode = 'checkFall'
+  } else if (mode == 'gameOver') {
+    PuyoImage.prepareBatankyu(frame);
+    mode = 'batankyu';
+  } else if (mode == 'batankyu') {
+    PuyoImage.batankyu(frame);
+    Player.batankyu();
   }
-  frame++;
+  frame = frame + 1;
   requestAnimationFrame(loop);
 }
